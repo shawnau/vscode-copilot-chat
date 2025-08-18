40416aa19acf99b53822a05f598336b8974fa741
Connor Peet <connor@peet.io>
2025-08-18 14:08:59 -0700

    edits: prep for gpt-5 apply-patch exclusive (#650)

--- src/extension/prompts/node/agent/agentInstructions.tsx
+++ src/extension/prompts/node/agent/agentInstructions.tsx
@@ -153,7 +153,7 @@ export class DefaultAgentPrompt extends PromptElement<DefaultAgentPromptProps> {
 					`}`
 				].join('\n')}
 			</Tag>}
-			{hasApplyPatchTool && <ApplyPatchInstructions {...this.props} />}
+			{hasApplyPatchTool && <ApplyPatchInstructions {...this.props} hasInsertEditTool={hasInsertEditTool} />}
 			{isGpt5 && hasTodoListTool && <TodoListToolInstructions {...this.props} />}
 			<NotebookInstructions {...this.props} />
 			<Tag name='outputFormatting'>
@@ -229,6 +229,7 @@ export class SweBenchAgentPrompt extends PromptElement<DefaultAgentPromptProps> 
 		const hasReplaceStringTool = !!this.props.availableTools?.find(tool => tool.name === ToolName.ReplaceString);
 		const hasEditFileTool = !!this.props.availableTools?.find(tool => tool.name === ToolName.EditFile);
 		const hasApplyPatchTool = !!this.props.availableTools?.find(tool => tool.name === ToolName.ApplyPatch);
+		const hasInsertEditTool = !!this.props.availableTools?.find(tool => tool.name === ToolName.EditFile);
 
 		return <InstructionMessage>
 			<Tag name="mostImportantInstructions">
@@ -365,7 +366,7 @@ export class SweBenchAgentPrompt extends PromptElement<DefaultAgentPromptProps> 
 					`}`
 				].join('\n')}
 			</Tag>}
-			{hasApplyPatchTool && <ApplyPatchInstructions {...this.props} />}
+			{hasApplyPatchTool && <ApplyPatchInstructions {...this.props} hasInsertEditTool={hasInsertEditTool} />}
 			<Tag name='outputFormatting'>
 				Use proper Markdown formatting in your answers. When referring to a filename or symbol in the user's workspace, wrap it in backticks.<br />
 				<Tag name='example'>
@@ -409,11 +410,11 @@ export class ApplyPatchFormatInstructions extends PromptElement {
 	}
 }
 
-class ApplyPatchInstructions extends PromptElement<DefaultAgentPromptProps> {
+class ApplyPatchInstructions extends PromptElement<DefaultAgentPromptProps & { hasInsertEditTool: boolean }> {
 	async render(state: void, sizing: PromptSizing) {
 		const isGpt5 = this.props.modelFamily?.startsWith('gpt-5') === true;
 		return <Tag name='applyPatchInstructions'>
-			To edit files in the workspace, use the {ToolName.ApplyPatch} tool. If you have issues with it, you should first try to fix your patch and continue using {ToolName.ApplyPatch}. If you are stuck, you can fall back on the {ToolName.EditFile} tool. But {ToolName.ApplyPatch} is much faster and is the preferred tool.<br />
+			To edit files in the workspace, use the {ToolName.ApplyPatch} tool. If you have issues with it, you should first try to fix your patch and continue using {ToolName.ApplyPatch}. {this.props.hasInsertEditTool && <>If you are stuck, you can fall back on the {ToolName.EditFile} tool, but {ToolName.ApplyPatch} is much faster and is the preferred tool.</>}<br />
 			{isGpt5 && <>Prefer the smallest set of changes needed to satisfy the task. Avoid reformatting unrelated code; preserve existing style and public APIs unless the task requires changes. When practical, complete all edits for a file within a single message.<br /></>}
 			The input for this tool is a string representing the patch to apply, following a special format. For each snippet of code that needs to be changed, repeat the following:<br />
 			<ApplyPatchFormatInstructions /><br />
